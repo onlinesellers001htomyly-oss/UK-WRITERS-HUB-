7f539314c3618cb06b228b93e0ed0e096635596f
onlinesellers001htomyly-oss <onlinesellers001htomyly@gmail.com>
2026-06-26 17:11:54 +0300

    Remove referral code generation import

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.get("/", (req, res) => {
 app.post("/register", (req, res) => {
 
     const {fullname,email,password,referral} = req.body;
-const { generateReferralCode } = require("./referral");
     const user = {
 
         id: users.length + 1,
