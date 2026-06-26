1c2f4426227ee5f19e02ca4a1a29203c3e3bd1ce
onlinesellers001htomyly-oss <onlinesellers001htomyly@gmail.com>
2026-06-26 16:19:30 +0300

    Create users.js

---
+++ users.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const db = require("./database");
+
+// Get all users
+router.get("/", (req, res) => {
+    res.json(db.users);
+});
+
+// Get one user by ID
+router.get("/:id", (req, res) => {
+
+    const id = Number(req.params.id);
+
+    const user = db.users.find(u => u.id === id);
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found."
+        });
+    }
+
+    res.json(user);
+});
+
+// Update user
+router.put("/:id", (req, res) => {
+
+    const id = Number(req.params.id);
+
+    const user = db.users.find(u => u.id === id);
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found."
+        });
+    }
+
+    Object.assign(user, req.body);
+
+    res.json({
+        success: true,
+        message: "User updated successfully.",
+        user
+    });
+
+});
+
+module.exports = router;
